478b3462ace3ab16ab9436efb589afb6b899cc88
sveinnoli <sveinnoli@hotmail.com>
2022-04-03 22:23:20 +0000

    day 5 and 10

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+let videoPlayer = document.querySelector('.player__video');
+let playerControls = document.querySelector('.player__controls');
+let playButton = document.querySelector('button[title="Toggle Play"');
+
+
+videoPlayer.play();
+videoPlayer.pause();
